d5564c347b4585d676f23ac1626d9650769467d3
Abhishek Kumar <abhishek.kumar12@snapdeal.com>
2015-07-29 13:58:55 +0530

    Created new project.

---
+++ Snapdeal Seller Zone/Snapdeal Seller ZoneTests/Snapdeal_Seller_ZoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  Snapdeal_Seller_ZoneTests.swift
+//  Snapdeal Seller ZoneTests
+//
+//  Created by Abhishek Kumar on 29/07/15.
+//  Copyright (c) 2015 Snapdeal. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Snapdeal_Seller_ZoneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
